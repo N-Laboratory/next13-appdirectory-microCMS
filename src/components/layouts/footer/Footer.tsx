b6de92820db2d7adf7179b0ca11326837b72a446
N-Laboratory <xnbaxx@gmail.com>
2023-03-20 12:40:52 +0900

    feat: add Footer.tsx to components/layouts/footer

---
+++ src/components/layouts/footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="bg-gray-900">
+      <footer className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="border-t border-gray-800 py-8 text-center text-sm text-gray-400">© 2023 Naoki Nakanishi All Rights Reserved.</div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
