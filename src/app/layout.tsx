962f6b5a331eca7e009056cbbfb52b3700de5e8f
N-Laboratory <xnbaxx@gmail.com>
2023-03-20 12:42:36 +0900

    refactor: set header and footer in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import './globals.css'
+import Header from '@/components/layouts/header/Header'
+import Footer from '@/components/layouts/footer/Footer'
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">
+        <Header/>
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
