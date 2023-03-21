9d3d606822659cf7177120262cebce2ea179275b
N-Laboratory <xnbaxx@gmail.com>
2023-03-21 12:28:07 +0900

    refactor: fix loading icon layout

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,12 +2,14 @@ import styles from "./loading.module.css"
 
 const Loading = () => {
   return (
-    <div className={styles.atomSpinner}>
-      <div className={styles.spinnerInner}>
-        <div className={styles.spinnerLine}></div>
-        <div className={styles.spinnerLine}></div>
-        <div className={styles.spinnerLine}></div>
-        <div className={styles.spinnerCircle}>&#9679;</div>
+    <div className="absolute w-full	bg-white z-50">
+      <div className={styles.atomSpinner}>
+        <div className={styles.spinnerInner}>
+          <div className={styles.spinnerLine}></div>
+          <div className={styles.spinnerLine}></div>
+          <div className={styles.spinnerLine}></div>
+          <div className={styles.spinnerCircle}>&#9679;</div>
+        </div>
       </div>
     </div>
   )
