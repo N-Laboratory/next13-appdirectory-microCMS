dd13ee6339ed74d9f9333270f97eed27638ff550
N-Laboratory <xnbaxx@gmail.com>
2023-03-21 23:04:57 +0900

    feat: create mobile header

--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,29 +1,39 @@
+"use client"
+
 import Link from 'next/link';
+import HeaderMobile from '@/components/layouts/header/HeaderMobile'
+import { useState } from 'react';
+import styles from './Header.module.css'
 
 const Header = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active)
+  }
+
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <header className="mb-8 flex items-center justify-end sm:justify-center py-4 md:mb-12 md:py-8 xl:mb-16 relative">
-          <Link href="/" className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl absolute left-0" aria-label="logo">
-            N-LAB
-          </Link>
+    <>
+      <div className="bg-white">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <header className="mb-8 flex items-center justify-end sm:justify-center py-4 md:mb-12 md:py-8 xl:mb-16 relative">
+            <Link href="/" className="text-black-800 inline-flex items-center gap-2.5 text-4xl sm:text-2xl font-bold md:text-3xl absolute left-0" aria-label="logo">
+              N-LAB
+            </Link>
 
-          <nav className="hidden gap-12 sm:flex">
-            <Link href="/" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Home</Link>
-            <Link href="/list" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">All Articles</Link>
-            <Link href="/about" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">About Me</Link>
-          </nav>
+            <nav className="hidden gap-12 sm:flex">
+              <Link href="/" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Home</Link>
+              <Link href="/list" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">All Articles</Link>
+              <Link href="/search" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">Search</Link>
+              <Link href="/about" className="text-lg font-semibold text-gray-600 transition duration-100 hover:text-indigo-500 active:text-indigo-700">About Me</Link>
+            </nav>
 
-          <button type="button" className="inline-flex items-center gap-2 rounded-lg bg-gray-200 px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 hover:bg-gray-300 focus-visible:ring active:text-gray-700 md:text-base sm:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
-            </svg>
-            Menu
-          </button>
-        </header>
+            <button onClick={handleClick} className={`${active && styles.active} ${styles.openbtn4} inline-flex items-center gap-2 rounded-lg px-2.5 py-2 text-sm font-semibold text-gray-500 ring-indigo-300 focus-visible:ring active:text-gray-700 md:text-base sm:hidden`}><span></span><span></span><span></span></button>
+          </header>
+        </div>
       </div>
-    </div>
+      {active && <HeaderMobile/>}
+    </>
   );
 };
 
