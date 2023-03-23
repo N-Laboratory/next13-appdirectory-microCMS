647826c277abb6bfb28c323966ab0738dd076071
N-Laboratory <xnbaxx@gmail.com>
2023-03-23 21:19:19 +0900

    refactor: add null check to microcms/client.ts

--- src/libs/microcms/client.ts
+++ src/libs/microcms/client.ts
@@ -1,6 +1,14 @@
 import { createClient } from 'microcms-js-sdk';
 
+if (!process.env.SERVICE_DOMAIN) {
+  throw new Error("SERVICE_DOMAIN is required");
+}
+
+if (!process.env.API_KEY) {
+  throw new Error("API_KEY is required");
+}
+
 export const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN!,
-  apiKey: process.env.API_KEY!,
+  serviceDomain: process.env.SERVICE_DOMAIN,
+  apiKey: process.env.API_KEY,
 });
