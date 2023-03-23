c4860ff2206dc9e5772828dd239afc0f432be6c2
N-Laboratory <xnbaxx@gmail.com>
2023-03-23 20:43:59 +0900

    chore: define article type for microCMS

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,14 @@
+export type ArticleList = {
+  contents: Article[]
+  totalCount?: number;
+  offset?: number;
+  limit?: number;
+};
+
 export type Article = {
   id: string;
   title: string;
-  detail?: string;
-  overview?: string;
-  createdDate?: string;
-  updatedDate?: string;
+  detail: string;
+  overview: string;
+  createdDate: string;
 };
