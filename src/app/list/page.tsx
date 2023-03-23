962eb085b40f7e825baa994d833584f2ea4588b5
N-Laboratory <xnbaxx@gmail.com>
2023-03-23 14:57:55 +0900

    refactor: change api method from fetch to swr/axios in app/list/page.tsx

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,67 +1,51 @@
 "use client"
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { type ArticleListResponse } from '../api/list/route';
-import { type Article } from '@/types';
+import { useSearchParams } from 'next/navigation';
 import Loading from '../loading';
+import useSWRImmutable  from "swr/immutable";
+import axios from 'axios';
+import Error from '../error/page';
 import Link from 'next/link';
+import { ArticleListResponse } from '../api/list/route';
+
+const fetcher = async (url: string, keyword: string) => await axios.post(url, { keyword }).then(res => res.data)
 
 // TODO ページング機能を実装する
 const List = () => {
-  const router = useRouter();
   const searchParams = useSearchParams();
-  const keyword = searchParams.get("keyword") ? searchParams.get("keyword") : "";
-  const [articleList, setArticleList] = useState<Article[]>([]);
-  const controller = new AbortController();
-  const signal = controller.signal;
+  const keyword = searchParams.get("keyword") ?? "";
+  const { data, error, isLoading } = useSWRImmutable<ArticleListResponse>(["/api/list", keyword], ([url, keyword]: [url: string, keyword: string]) => fetcher(url, keyword), {
+    shouldRetryOnError: false,
+  });
 
-  // TODO suspenceを用いてuseEffectを使用しない実装に置き換える
-  useEffect(() => {
-    (async () => {
-      const response = await fetch('/api/list', {
-        method: 'POST',
-        body: JSON.stringify({ keyword }),
-        signal: signal
-      });
-
-      if (response.status !== 200) {
-        router.push('/error');
-        return;
-      }
-
-      const data: ArticleListResponse = await response.json()
-      setArticleList(data.articleList)
-    })();
-    return () => {
-      // 画面遷移時にフェッチをキャンセル
-      controller.abort();
-    };
-  }, []);
-
-  if (!articleList.length) {
-    return (
-      <Loading />
-    )
+  if (error) {
+    return <Error/>;
   }
+
+  if (isLoading) {
+    return <Loading/>;
+  }
+
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12 flex-grow">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
       <div className="mb-10 md:mb-16">
         <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">記事一覧</h2>
       </div>
-
+      { data && data.articleList && data.articleList.length ?
         <div className="grid gap-4 sm:grid-cols-2 md:gap-8 xl:grid-cols-3">
-          {
-            articleList.map(article => (
+          { data.articleList.map((article) => (
               <div key={article.id} className="flex flex-col rounded-lg border p-4 md:p-6 hover:bg-slate-200">
                 <h3 className="mb-2 text-lg font-semibold md:text-xl">{article.title}</h3>
                 <p className="mb-4 text-gray-500">{article.overview}</p>
-                <Link href={`/articles/${article.id}`} className="mt-auto font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">More</Link>
+                <Link href={`/articles/${encodeURIComponent(article.title)}`} className="mt-auto font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">More</Link>
               </div>
             ))
           }
         </div>
+        :
+        <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">記事が存在しません</h2>
+      }
       </div>
     </div>
   )
