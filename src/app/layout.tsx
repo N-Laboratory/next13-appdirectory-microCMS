87572f6ece72827ef109ca1379aa103462ae11e5
N-Laboratory <xnbaxx@gmail.com>
2023-03-30 15:59:43 +0900

    refactor: add new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import './globals.css'
 import Header from '@/components/layouts/header/Header'
 import Footer from '@/components/layouts/footer/Footer'
+import type { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+}
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja-JP'>
