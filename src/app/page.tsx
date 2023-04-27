5cf8088d248e374f9909db3e7aa781080a5687b0
N-Laboratory <xnbaxx@gmail.com>
2023-04-27 18:54:03 +0900

    refactor: fix page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ const Home = async () => {
         </div>
       </div>
 
-      <div className='bg-white py-6 sm:py-8 lg:py-12'>
+      <div className='bg-white py-6 sm:py-8 lg:py-12 xl:px-24'>
         <div className='mx-auto max-w-screen-2xl px-4 md:px-8'>
           <div className='mb-10 md:mb-16'>
             <h2 className='mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl'>
