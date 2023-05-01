465476e5f180a74f1d5e537d327693fbc6f363ce
N-Laboratory <xnbaxx@gmail.com>
2023-05-01 15:43:05 +0900

    chore: install @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import Header from '@/components/layouts/header/Header'
 import Footer from '@/components/layouts/footer/Footer'
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react' ;
 
 export const metadata: Metadata = {
   icons: {
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         {children}
         <Footer />
+        <Analytics/>
       </body>
     </html>
   )
