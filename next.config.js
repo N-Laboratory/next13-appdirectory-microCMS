e75959967727c58b49b74d0e2c6183652d9c6980
N-Laboratory <xnbaxx@gmail.com>
2023-05-02 11:23:47 +0900

    refactor: delete cache custom header

--- next.config.js
+++ next.config.js
@@ -1,42 +1,5 @@
-const cacheHeaders = [
-  {
-    key: 'Cache-Control',
-    value: 'max-age=0, s-maxage=86400',
-  }
-];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        // URLを直接入力して遷移した場合
-        source: '/',
-        headers: cacheHeaders,
-      },
-      {
-        // next/link や next/routerで遷移した場合
-        source: '/_next/data/:hash/',
-        headers: cacheHeaders,
-      },
-      {
-        source: '/list',
-        headers: cacheHeaders,
-      },
-      {
-        source: '/_next/data/:hash/list',
-        headers: cacheHeaders,
-      },
-      {
-        source: '/articles/:id*',
-        headers: cacheHeaders,
-      },
-      {
-        source: '/_next/data/:hash/articles/:id*',
-        headers: cacheHeaders,
-      },
-    ];
-  },
   experimental: {
     appDir: true,
   },
