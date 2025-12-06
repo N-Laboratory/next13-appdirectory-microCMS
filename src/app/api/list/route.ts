78230eb3e29e1af34cf37043c0b72febe1b6093f
N-Laboratory <xnbaxx@gmail.com>
2025-12-06 17:16:46 +0900

    refactor: modify /api/list api

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -3,49 +3,35 @@ import { htmlspecialchars } from '@/features/common/sanitize'
 import { NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 import { Article } from '@/types'
-import { getArticleList, getArticleListByKeyword } from '@/libs/microcms/client'
+import { getArticleList } from '@/libs/microcms/client'
+
+export type ArticleListResponse = {
+  articleList: Article[]
+}
 
 const userSchema = z.object({
   keyword: z.string(),
 })
 
-export type ArticleListResponse = {
-  articleListPerPage: Article[][]
-  pageTotal: number
-}
-
 export const POST = async (request: NextRequest) => {
   const body = await request.json()
   const result = userSchema.safeParse(body)
-  const articleListPerPage: Article[][] = []
 
   if (!result.success) {
-    return NextResponse.json({ articleListPerPage }, { status: 400 })
+    return NextResponse.json([], { status: 400 })
   }
 
   const keyword = htmlspecialchars(result.data.keyword.trim())
-  const articleList =
-    keyword == ''
-      ? await getArticleList('id,title,overview,svgPath,createdDate')
-      : await getArticleListByKeyword(keyword, 'id,title,overview,svgPath,createdDate')
-  if (!articleList) {
-    return NextResponse.json({ articleListPerPage }, { status: 500 })
-  }
+  const articleList = (
+    await getArticleList('id,title,overview,svgPath,category,createdDate', keyword)
+  )?.contents
 
-  const articleTotal = articleList.totalCount ?? 1
-  const pageTotal = Math.ceil(articleTotal / 5)
-  if (articleList.contents.length) {
-    let start = 0
-    let end = 5
-    for (let index = 0; index < pageTotal; index++) {
-      articleListPerPage.push(articleList.contents.slice(start, end))
-      start += 5
-      end += 5
-    }
+  if (!articleList) {
+    return NextResponse.json([], { status: 500 })
   }
 
   return NextResponse.json(
-    { articleListPerPage, pageTotal },
+    { articleList },
     {
       status: 200,
     },
